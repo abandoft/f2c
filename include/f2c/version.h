812fd25082f05f317ca602e084820a7d8900c6dc
moluopro <moluopro@qq.com>
2026-07-20 02:14:26 +0800

    release: prepare 1.16.0

--- include/f2c/version.h
+++ include/f2c/version.h
@@ -3,8 +3,8 @@
 
 /* The authoritative project version consumed by both C and CMake. */
 #define F2C_VERSION_MAJOR 1
-#define F2C_VERSION_MINOR 15
+#define F2C_VERSION_MINOR 16
 #define F2C_VERSION_PATCH 0
-#define F2C_VERSION_STRING "1.15.0"
+#define F2C_VERSION_STRING "1.16.0"
 
 #endif
