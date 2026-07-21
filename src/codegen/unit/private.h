fb7f91913eb0313b71131a86d1c19f3ce52daa23
moluopro <moluopro@qq.com>
2026-07-21 18:20:54 +0800

    refactor(unit): declare automatic storage helpers

--- src/codegen/unit/private.h
+++ src/codegen/unit/private.h
@@ -9,6 +9,11 @@ const char *f2c_unit_function_return_type(Unit *unit);
 void f2c_unit_emit_named_signature(Buffer *output, Unit *unit, const char *name,
                                    int restricted_arguments);
 void f2c_unit_emit_signature(Buffer *output, Unit *unit);
+void f2c_unit_emit_automatic_array_declaration(Buffer *output, Unit *unit, Symbol *symbol,
+                                               int depth);
+void f2c_unit_emit_automatic_array_allocation(Buffer *output, Unit *unit, Symbol *symbol,
+                                              int depth);
+void f2c_unit_emit_automatic_array_cleanup(Buffer *output, Unit *unit, Symbol *symbol, int depth);
 char *f2c_unit_data_array_initializer(Unit *unit, const Symbol *symbol);
 int f2c_unit_expression_is_character_temporary(const F2cExpr *expression);
 int f2c_unit_statement_is_function_definition(const Unit *unit, size_t statement);
