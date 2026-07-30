c91fc5ab500616d7e02944ece700c70bfefab1c3
moluopro <moluopro@qq.com>
2026-07-30 17:52:20 +0800

    test(reduction): diff scalar semantics against Fortran

--- test/fixtures/reduction_intrinsics.f90
+++ test/fixtures/reduction_intrinsics.f90
@@ -2,9 +2,23 @@ program reduction_intrinsics
   implicit none
   integer :: values(5)
   logical :: flags(5)
+  integer(kind=8) :: wide_values(2)
+  real :: real_values(2)
+  complex :: complex_values(2), complex_weights(2)
+  complex(kind=8) :: wide_complex_values(2), wide_complex_weights(2)
+  logical(kind=1) :: compact_flags(3)
+  logical(kind=8) :: wide_flags(3)
 
   values = [2, 3, 4, 5, 6]
   flags = [.true., .false., .true., .true., .false.]
+  wide_values = [2_8, 3_8]
+  real_values = [4.0, 5.0]
+  complex_values = [(1.0, 2.0), (3.0, 4.0)]
+  complex_weights = [(5.0, 6.0), (7.0, 8.0)]
+  wide_complex_values = [(1.0_8, 2.0_8), (3.0_8, 4.0_8)]
+  wide_complex_weights = [(5.0_8, 6.0_8), (7.0_8, 8.0_8)]
+  compact_flags = [.true., .false., .true.]
+  wide_flags = [.false., .true., .true.]
 
   if (sum(values) /= 20) stop 1
   if (sum(values(5:1:-2)) /= 12) stop 2
@@ -17,4 +31,11 @@ program reduction_intrinsics
   if (.not. any(flags)) stop 9
   if (all(flags)) stop 10
   if (dot_product(values, [1, 2, 3, 4, 5]) /= 70) stop 11
+  if (dot_product(wide_values, real_values) /= 23.0) stop 12
+  if (sum(complex_values) /= (4.0, 6.0)) stop 13
+  if (product(complex_values) /= (-5.0, 10.0)) stop 14
+  if (dot_product(complex_values, complex_weights) /= (70.0, -8.0)) stop 15
+  if (dot_product(wide_complex_values, wide_complex_weights) /= (70.0_8, -8.0_8)) stop 16
+  if (.not. dot_product(compact_flags, wide_flags)) stop 17
+  print '(A)', 'reduction intrinsic differential passed'
 end program reduction_intrinsics
