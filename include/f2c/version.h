c429f210294fd3025760fa31488b4d6356643baf
moluopro <moluopro@qq.com>
2026-07-31 05:33:32 +0800

    chore(version): prepare 1.29.0

--- include/f2c/version.h
+++ include/f2c/version.h
@@ -3,8 +3,8 @@
 
 /* The authoritative project version consumed by both C and CMake. */
 #define F2C_VERSION_MAJOR 1
-#define F2C_VERSION_MINOR 28
+#define F2C_VERSION_MINOR 29
 #define F2C_VERSION_PATCH 0
-#define F2C_VERSION_STRING "1.28.0"
+#define F2C_VERSION_STRING "1.29.0"
 
 #endif
