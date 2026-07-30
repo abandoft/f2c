fd46779d74c133956f1106fee5579034cee83f56
moluopro <moluopro@qq.com>
2026-07-30 17:57:15 +0800

    test(reduction): cover filtered scalar reductions

--- test/fixtures/reduction_intrinsics.f90
+++ test/fixtures/reduction_intrinsics.f90
@@ -8,6 +8,8 @@ program reduction_intrinsics
   complex(kind=8) :: wide_complex_values(2), wide_complex_weights(2)
   logical(kind=1) :: compact_flags(3)
   logical(kind=8) :: wide_flags(3)
+  integer :: tied_values(4)
+  logical(kind=1) :: selected(4)
 
   values = [2, 3, 4, 5, 6]
   flags = [.true., .false., .true., .true., .false.]
@@ -19,6 +21,8 @@ program reduction_intrinsics
   wide_complex_weights = [(5.0_8, 6.0_8), (7.0_8, 8.0_8)]
   compact_flags = [.true., .false., .true.]
   wide_flags = [.false., .true., .true.]
+  tied_values = [2, 9, 9, 4]
+  selected = [.false., .true., .true., .false.]
 
   if (sum(values) /= 20) stop 1
   if (sum(values(5:1:-2)) /= 12) stop 2
@@ -37,5 +41,16 @@ program reduction_intrinsics
   if (dot_product(complex_values, complex_weights) /= (70.0, -8.0)) stop 15
   if (dot_product(wide_complex_values, wide_complex_weights) /= (70.0_8, -8.0_8)) stop 16
   if (.not. dot_product(compact_flags, wide_flags)) stop 17
+  if (sum(tied_values, mask=selected) /= 18) stop 18
+  if (product(tied_values, mask=selected) /= 81) stop 19
+  if (maxval(tied_values, mask=selected) /= 9) stop 20
+  if (minval(tied_values, mask=selected) /= 9) stop 21
+  if (sum(tied_values, mask=.false.) /= 0) stop 22
+  if (product(tied_values, mask=.false.) /= 1) stop 23
+  if (maxloc(tied_values, dim=1, mask=selected, kind=8) /= 2_8) stop 24
+  if (maxloc(tied_values, dim=1, mask=selected, back=.true.) /= 3) stop 25
+  if (minloc(tied_values, dim=1, mask=selected, back=.true.) /= 3) stop 26
+  if (count(selected, kind=1) /= 2_1) stop 27
+  if (sum(complex_values, mask=[.true., .false.]) /= (1.0, 2.0)) stop 28
   print '(A)', 'reduction intrinsic differential passed'
 end program reduction_intrinsics
